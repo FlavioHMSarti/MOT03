50fec562ffdf2f1f3436a7dbb8b92dbb428de170
FlavioHMSarti <63512638+FlavioHMSarti@users.noreply.github.com>
2020-09-13 22:21:30 +0200

    update code
    Tous les points (sauf le 9). Il faut encore calculer la chaleur perdue totale

--- code_matlab_modelisation.m
+++ code_matlab_modelisation.m
@@ -18,6 +18,7 @@ cv_p = @(Temp) -6/10^8*Temp^2 + 0.0003*Temp + 0.6638;
 gamma_p = @(Temp) 2/10^8*Temp^2 - 0.0001*Temp + 1.4104;
 rp = 287.0473442;
 
+PCI = 42722.66063; % kJ/kg
 
 
 %% Hyp
@@ -39,7 +40,11 @@ taux_comp = 21.8; % moteur 4b
 
 n_comp = 0.8;
 pi_comp = 1.8;
+n_turb = 0.8;
 pi_turb = 1.4; 
+
+n_meca = 0.875;
+n_comb = 0.98;
 
 %% Point 2
 
@@ -54,11 +59,11 @@ tol = 1;                %DT = 1K
 i = 0;
 
 while (DT > tol && i < 10000)
+   i = i+1;
    Tmoy = (T2_is + T(1))/2;  
    gamma = gamma_a(Tmoy); 
    T2_is = T(1)*(p(2)/p(1))^((gamma-1)/gamma); 
-    i = i+1;
-    DT = abs(T2_is - 2*Tmoy + T(1));
+   DT = abs(T2_is - 2*Tmoy + T(1));
 end
 T(2) = T(1) + (T2_is-T(1))/n_comp;
 
@@ -69,13 +74,16 @@ p(3)=p(2)-Dp_ech;
 
 %% Point 3'
 
-% 3' -> dans le moteur, soupape ferm?
+% 3' -> dans le moteur, soupape ferme
 r3p = ra;
 
 Cu = pi*D^2*C/4;
 V_max = taux_comp * Cu / (taux_comp - 1); % Point mort bas
 V_min = Cu/(taux_comp-1);
 
+% Point 3’ : Les gaz résiduels et les gaz frais sont mélangés à la
+% pression P3’. On calcule d'abord 3' sans gaz résiduel et on le
+% modifie après dans une boucle
 T3p = T(3);
 p3p = p(3); % pas de perte de charge apres le soupape
 m3p = p3p*10^5*V_max/r3p/T3p;
@@ -96,10 +104,9 @@ p1 = polyfit(V_ln,p_ln,1);
 n_comp = 1.34; % Excel
 p(4) = p3p*taux_comp^n_comp;
 T(4) = p(4)*10^5*V_min/m3p/ra;
+V(4) = V_min;
 
 %% Point 4'
-rp = 287.0473442; % Excel
-
 p4p = max(p_cycle);
 m4p = m3p;
 r4p = rp; % ******
@@ -168,41 +175,41 @@ end
 %% Boucle T3p
 
 f = (1/taux_comp)*(T(3)/T(7))*(p(7)/p3p);
-tol_DT3p = 1; % 1K
+tol = 1; % 1K
 j = 0;
 
 r3p = f*rp+(1-f)*ra; % Diesel
 T0 = 273.15;
-dif = 100;
+DT = 100;
 T3p_old = T3p;
 
 
-while (dif > tol_DT3p && j < 10^3)
+while (DT > tol && j < 10^3)
     j=j+1;
     T03 = (T0+T3p)/2;
     T07 = (T0+T7p)/2;
     cp_03 = f*cp_p(T03)+(1-f)*cp_a(T03);
     T3pn = (f*cp_p(T07)*(T7p-T0)+(1-f)*cp_a(T03)*(T(3)-T0))/cp_03+T0;
-    dif = abs(T3pn-T3p_old);
+    DT = abs(T3pn-T3p_old);
     T3p_old = T3pn;
     m3pn = p3p*10^5*V_max/r3p/T3p;
     f = p(7)*10^5*V_min/m3pn/rp/T(7);
 end
 
 %% 3' - 4
-Tn = zeros(1,8); pn = T; Vn = T;
-r4n = f*rp+(1-f)*ra; %diesel
-Tn(4) = p(4)*10^5*V_min/m3pn/r4n;
+Tn = zeros(1,8); pn = Tn; Vn = Tn;
+r4 = f*rp+(1-f)*ra; %diesel
+Tn(4) = p(4)*10^5*V_min/m3pn/r4;
 m4pn = m3pn;
 
 % # 1er BOUCLAGE ####################################################
 % m=cst = m3p
 %% isochore 4-4'
-T4pn = p4p*10^5*V_min/m3pn/rp;
+T4pn = p4p*10^5*V_min/m4pn/rp;
 Tmoy = 0.5*(T0+T4pn);
 Tmoy2 = 0.5*(T0+Tn(4));
 cv = f*cv_p(Tmoy2)+(1-f)*cv_a(Tmoy2);
-Qv = m4pn*cv_p(Tmoy)*(T4pn-T0)-m3pn*cv*(Tn(4)-T0);  
+Qv = m4pn*cv_p(Tmoy)*(T4pn-T0)-m3pn*cv*(Tn(4)-T0);
 
 %% isobare 4'-4''
 m4ppn = m3pn;
@@ -213,50 +220,156 @@ Qp = m4ppn * cp_p(Tmoy)*(T4ppn-T4pn);
 %% isotherme 4''-5
 Tn(5) = p(5)*10^5*V(5)/m3pn/rp;
 DT_45 = abs(Tn(5)-T4ppn);
-Qt = m3pn * rp * log(V(5)/V4pp);
+Qt = m3pn * rp * Tn(5) * log(V(5)/V4pp);
 
 %% Qapp
-Qpp = Qv+Qp+Qt;
+Qapp = Qv+Qp+Qt;
 
 %fraction de gaz brules
 %xbi = Qi/Qapp
-xb4p = Qv/Qpp;
-xb4pp = (Qv+Qp)/Qpp;
+xv = Qv/Qapp;
+xp = (Qv+Qp)/Qapp;
 
 
-% # 2eme BOUCLAGE #####################################################3
+% # 2eme BOUCLAGE #####################################################
 % m != cst (Diesel)
 
 %% 4-4'
-m4pn = (1+F*xb4p)*m3pn;
-T4pn = p4p*V_min/m4pn/r3pn;
+m4pn = (1+F*xv)*m3pn;
+r4p = (1-xv)*rf+xv*rp;
+T4pn = p4p*V_min/m4pn/r4p;
 Tmoy = 0.5*(T0+T4pn);
-cv = (1-xb4pp)*cv_a(Tmoy)+xb4p*cv_p(Tmoy);
-cv2 = f*cv_p(Tmoy)+(1-f)*cv_a(Tmoy);
-Qv = m4pn*cv(Tmoy)*(T4pn-T0)-m3pn*cv2*(Tn(4)-T0); 
+Tmoy2 = 0.5*(T0+Tn(4));
+cv = (1-xv)*cv_f(Tmoy)+xv*cv_p(Tmoy);
+cv2 = f*cv_p(Tmoy2)+(1-f)*cv_a(Tmoy2);
+Qv = m4pn * cv *(T4pn-T0)-m3pn*cv2*(Tn(4)-T0); 
 
 %% isobare 4'-4''
-m4ppn = (1+F*xb4pp)*m3pn;
-r4ppn = (1-xb4pp)*rf+xb4pp*rp;
+m4ppn = (1+F*xp)*m3pn;
+r4pp = (1-xp)*rf+xp*rp;
+T4ppn = p4pp*10^5*V4pp/m4ppn/r4pp;
 
-T4ppn = p4pp*10^5*V4pp/m4ppn/r4ppn;
-Tmoy = 0.5*(T4pnn+T0);
+Tmoy = 0.5*(T4ppn+T0);
 Tmoy2 = 0.5*(T4pn+T0);
-cp = (1-xb4pp)*cp_f(Tmoy)+xb4pp*cp_p(Tmoy);
-cp2 = (1-xb4p)*cp_f(Tmoy)+xb4p*cp_p(Tmoy);
-Qp = m4ppn * cp*(T4ppn-T0)-m4pn*cp2*(T4pn-T0);
+cp = (1-xp)*cp_f(Tmoy)+xp*cp_p(Tmoy);
+cp2 = (1-xv)*cp_f(Tmoy2)+xv*cp_p(Tmoy);
+Qp = m4ppn * cp * (T4ppn-T0) - m4pn * cp2 * (T4pn-T0);
 
 %% isotherme 4''-5
 Vn(5)=V(5);
 m5n = (1+F)*m3pn;
-r5n = rp;
-Tn(5) = p(5)*V(5)/m5n/r5n;
+r5 = rp;
+Tn(5) = p(5)*10^5*V(5)/m5n/r5;
+
 Tmoy = 0.5*(T0+Tn(5));
 Tmoy2 = 0.5*(T0+T4ppn);
-cv = (1-xb4pp)*cv_f(Tmoy2)+xb4pp*cv_p(Tmoy2);
-Qt = m5n*cv_p(Tmoy)*(Tn(5)-T0)-m4ppn*cv*(T4ppn-T0)+0.5*(m4ppn*r4ppn+m5n*r5n)*Tn(5)*log(Vn(5)/V4pp);
+cv = (1-xp)*cv_f(Tmoy2)+xp*cv_p(Tmoy2);
+Qt = m5n*cv_p(Tmoy)*(Tn(5)-T0)-m4ppn*cv*(T4ppn-T0)+0.5*(m4ppn*r4pp+m5n*r5)*Tn(5)*log(Vn(5)/V4pp);
 
-Qapp = Qv+Qp+Qt;
+Qappn = Qv+Qp+Qt;
+
+%% 5-6
+Tn(6) = p(6)*10^5*V(6)/m5n/rp;
+
+%% 6 - 7
+
+DT = 100;
+tol = 1;                %DT = 1K
+i = 0;
+gamma = gamma_p(Tn(6));
+Tn(7) = (inv(gamma)+(gamma-1)/gamma*(p(7)/p(6)))*Tn(6);
+
+while (DT > tol && i < 10000)
+   Tmoy = (Tn(6) + Tn(7))/2;
+   gamma = gamma_p(Tmoy); 
+   Tn(7) = (inv(gamma)+(gamma-1)/gamma*(p(7)/p(6)))*T(6);
+   i = i+1;
+   DT = abs(Tn(7) - 2*Tmoy + Tn(6));
+end
+
+%% 7' (2)
+
+DT = 100;
+tol = 1;               
+i = 0;
+gamma = gamma_p(Tn(7));
+T7pn = Tn(7)*(p(7)/p7p)^((1-gamma)/gamma);
+
+while (DT > tol && i < 10000)
+   Tmoy = (Tn(7) + T7pn)/2;
+   gamma = gamma_p(Tmoy);
+   T7pn = Tn(7)*(p(7)/p7p)^((1-gamma)/gamma);
+   i = i+1;
+   DT = abs(Tn(7) - 2*Tmoy + T7pn);
+end
+
+%% Boucle T3pn
+
+%on a f deja
+tol = 1; % 1K
+j = 0;
+
+r3p = f*rp+(1-f)*ra; % Diesel
+T0 = 273.15;
+DT = 100;
+T3p_old = T3pn;
+fn = f;
 
 
-% Faire 5-6 et 6-7
+while (DT > tol && j < 10^3)
+    j=j+1;
+    T03 = (T0+T3pn)/2;
+    T07 = (T0+T7pn)/2;
+    cp_03 = fn*cp_p(T03)+(1-fn)*cp_a(T03);
+    T3pn = (fn*cp_p(T07)*(T7pn-T0)+(1-fn)*cp_a(T03)*(T(3)-T0))/cp_03+T0;
+    DT = abs(T3pn-T3p_old);
+    T3p_old = T3pn;
+    m3pn = p3p*10^5*V_max/r3p/T3pn;
+    fn = p(7)*10^5*V_min/m3pn/rp/Tn(7);
+end
+
+Df = abs(fn-f)/f*100;
+
+%% 7 - 8
+p(8) = p(7)/pi_turb;
+
+DT = 100;
+tol = 1;                %DT = 1K
+i = 0;
+
+gamma = gamma_p(Tn(7));
+T8_is = Tn(7)*(p(8)/p(7))^((gamma-1)/gamma); 
+
+while (DT > tol && i < 10000)
+   i = i+1;
+   Tmoy = (T8_is + Tn(7))/2;  
+   gamma = gamma_p(Tmoy); 
+   T8_is = Tn(7)*(p(8)/p(7))^((gamma-1)/gamma); 
+   DT = abs(T8_is - 2*Tmoy + Tn(7));
+end
+Tn(8) = Tn(7) + (T8_is-Tn(7))/n_turb;
+
+%% Bilan Thermique
+
+%qcarb = n_comb * (1-fn) * F * PCI * 10^3 * m3pn;
+qcarb = Qappn;
+
+Tmoy = 0.5*(T(1)+T(2));
+Tmoy2 = 0.5*(Tn(7)+Tn(8));
+qcomp = 0; %hypothese: compression adiabatique au compresseur
+Wturb = m3pn * cp_p(Tmoy)*(Tn(8)-Tn(7));
+%qturb = -qcomp*cp_a(Tmoy)*(T(2)-T(1))/n_meca/cp_p(Tmoy2)/(Tn(8)-Tn(7));
+qturb = Wturb*(1-n_meca);
+qwaste = qcomp + qcarb - qturb;
+
+%% 7 - 8'
+p8p = p_atm;
+T8p = Tn(7);
+
+%% 8' - 9
+% p(9) = p_atm;
+% Tn(9) = (1-qwaste/qmoteur)*Tn(8)+qwaste/qmoteur*T8p;
+
+Tn(Tn==0)=T(Tn==0);
+pn(pn==0)=p(pn==0);
+Vn(Vn==0)=V(Vn==0);
