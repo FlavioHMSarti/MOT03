b4d7374e7eacb4e6232ba83289837eb7ce83cc36
FlavioHMSarti <63512638+FlavioHMSarti@users.noreply.github.com>
2020-09-18 17:00:27 +0200

    fin séance 18/09
    avant de finir le calcul a partir de la chaleur de reaction

--- code_matlab_modelisation.m
+++ code_matlab_modelisation.m
@@ -3,22 +3,26 @@ close
 clc
 
 %% Excel
-cp_a = @(Temp) 0.0002*Temp + 0.9449;
-cv_a = @(Temp) 0.0002*Temp + 0.6566;
+cp_a = @(Temp) 0.202*Temp + 944.9;
+cv_a = @(Temp) 0.202*Temp + 656.59;
 gamma_a = @(Temp) -9/10^5*Temp + 1.4284;
 ra = 288.2801664;
 
-cp_f = @(Temp) 0.0003*Temp + 0.936;
-cv_f = @(Temp) 0.0003*Temp + 0.6585;
+cp_f = @(Temp) 0.3255*Temp + 935.96;
+cv_f = @(Temp) 0.3255*Temp + 658.51;
 gamma_f = @(Temp) -1/10^4*Temp + 1.4006;
 rf = 277.4513479;
 
-cp_p = @(Temp) -6/10^8*Temp^2 + 0.0003*Temp + 0.9508;
-cv_p = @(Temp) -6/10^8*Temp^2 + 0.0003*Temp + 0.6638;
+cp_p = @(Temp) -6/10^5*Temp^2 + 0.3304*Temp + 950.81;
+cv_p = @(Temp) -6/10^5*Temp^2 + 0.3304*Temp + 663.76;
 gamma_p = @(Temp) 2/10^8*Temp^2 - 0.0001*Temp + 1.4104;
 rp = 287.0473442;
 
 PCI = 42722.66063; % kJ/kg
+phi = 0.7;
+psi_s = 14.32288;
+
+
 
 
 %% Hyp
@@ -30,7 +34,7 @@ COF = 0.8;
 Dp_ech = 10/1000;
 
 % F = phi/psi_s
-F = 0.7/14.32288;
+F = phi/psi_s;
 
 %% Moteur
 
@@ -38,7 +42,7 @@ D = 79.5/1000;
 C = 80.5/1000;
 taux_comp = 21.8; % moteur 4b
 
-n_comp = 0.8;
+n_compresseur = 0.8;
 pi_comp = 1.8;
 n_turb = 0.8;
 pi_turb = 1.4; 
@@ -68,7 +72,7 @@ while (DT > tol && i < 10000)
    T2_is = T(1)*(p(2)/p(1))^((gamma-1)/gamma); 
    DT = abs(T2_is - 2*Tmoy + T(1));
 end
-T(2) = T(1) + (T2_is-T(1))/n_comp;
+T(2) = T(1) + (T2_is-T(1))/n_compresseur;
 
 %% Point 3
 
@@ -195,6 +199,7 @@ while (DT > tol && j < 10^3)
     f = p(7)*10^5*V_min/m3pn/rp/T(7);
 end
 
+
 %% 3' - 4
 Tn = zeros(1,8); pn = Tn; Vn = Tn;
 r4 = f*rp+(1-f)*ra; %diesel
@@ -264,7 +269,6 @@ Tmoy = 0.5*(T0+Tn(5));
 Tmoy2 = 0.5*(T0+T4ppn);
 cv = (1-xp)*cv_f(Tmoy2)+xp*cv_p(Tmoy2);
 Qt = m5n*cv_p(Tmoy)*(Tn(5)-T0)-m4ppn*cv*(T4ppn-T0)+0.5*(m4ppn*r4pp+m5n*r5)*Tn(5)*log(Vn(5)/V4pp);
-
 Qappn = Qv+Qp+Qt;
 
 %% 5-6
@@ -289,49 +293,69 @@ while (DT > tol && i < 10000)
 end
 
 
-%% 7' (2) pas besoin?
+% %% 7' (2) pas besoin?
+% 
+% DT = 100;
+% tol = 1;               
+% i = 0;
+% gamma = gamma_p(Tn(7));
+% T7pn = Tn(7)*(p(7)/p7p)^((1-gamma)/gamma);
+% 
+% while (DT > tol && i < 10000)
+%    Tmoy = (Tn(7) + T7pn)/2;
+%    gamma = gamma_p(Tmoy);
+%    T7pn = Tn(7)*(p(7)/p7p)^((1-gamma)/gamma);
+%    i = i+1;
+%    DT = abs(Tn(7) - 2*Tmoy + T7pn);
+% end
+% %% Boucle T3pn pas besoin?
+% 
+% %on a f deja
+% tol = 1; % 1K
+% j = 0;
+% 
+% r3p = f*rp+(1-f)*ra; % Diesel
+% T0 = 273.15;
+% DT = 100;
+% T3p_old = T3pn;
+% fn = f;
+% 
+% 
+% while (DT > tol && j < 10^3)
+%     j=j+1;
+%     T03 = (T0+T3pn)/2;
+%     T07 = (T0+T7pn)/2;
+%     cp_03 = fn*cp_p(T03)+(1-fn)*cp_a(T03);
+%     T3pn = (fn*cp_p(T07)*(T7pn-T0)+(1-fn)*cp_a(T03)*(T(3)-T0))/cp_03+T0;
+%     DT = abs(T3pn-T3p_old);
+%     T3p_old = T3pn;
+%     m3pn = p3p*10^5*V_max/r3p/T3pn;
+%     fn = p(7)*10^5*V_min/m3pn/rp/Tn(7);
+% end
+% Df = abs(fn-f)/f*100;
 
-DT = 100;
-tol = 1;               
-i = 0;
-gamma = gamma_p(Tn(7));
-T7pn = Tn(7)*(p(7)/p7p)^((1-gamma)/gamma);
-
-while (DT > tol && i < 10000)
-   Tmoy = (Tn(7) + T7pn)/2;
-   gamma = gamma_p(Tmoy);
-   T7pn = Tn(7)*(p(7)/p7p)^((1-gamma)/gamma);
-   i = i+1;
-   DT = abs(Tn(7) - 2*Tmoy + T7pn);
-end
-%% Boucle T3pn pas besoin?
-
-%on a f deja
-tol = 1; % 1K
-j = 0;
-
-r3p = f*rp+(1-f)*ra; % Diesel
-T0 = 273.15;
-DT = 100;
-T3p_old = T3pn;
-fn = f;
-
-
-while (DT > tol && j < 10^3)
-    j=j+1;
-    T03 = (T0+T3pn)/2;
-    T07 = (T0+T7pn)/2;
-    cp_03 = fn*cp_p(T03)+(1-fn)*cp_a(T03);
-    T3pn = (fn*cp_p(T07)*(T7pn-T0)+(1-fn)*cp_a(T03)*(T(3)-T0))/cp_03+T0;
-    DT = abs(T3pn-T3p_old);
-    T3p_old = T3pn;
-    m3pn = p3p*10^5*V_max/r3p/T3pn;
-    fn = p(7)*10^5*V_min/m3pn/rp/Tn(7);
-end
-Df = abs(fn-f)/f*100;
+Tn(Tn==0)=T(Tn==0);
+pn(pn==0)=p(pn==0);
+Vn(Vn==0)=V(Vn==0);
+% Tn=Tn'; pn=pn'; Vn=Vn';
+% T=T'; p=p'; V=V';
 
 %% 7 - 8
-p(8) = p(7)/pi_turb;
+tol_q = 15/100; %qwaste/qturb
+j = 0;
+qwaste = 1; qturb = 1;
+pi_turbn = pi_turb;
+rapport_q_old = 0;
+
+while(qwaste/qturb > tol_q  && j < 10^5)
+    
+    if (rapport_q_old > qwaste/qturb)
+        pi_turbn = pi_turb*(1-0.1);
+    else
+        pi_turbn = pi_turb*(1+0.1);
+    end
+
+p(8) = p(7)/pi_turbn;
 
 DT = 100;
 tol = 1;                %DT = 1K
@@ -355,37 +379,149 @@ Tmoy2 = 0.5*(Tn(7)+Tn(8));
 
 m3 = p3p*10^5*V_max/r3p/T3pn*(1-f);
 qcomp = m3 * N/120*Ncyl;
-qturb = -qcomp*cp_a(Tmoy)*(T(2)-T(1))/(n_meca*(cp_p(Tmoy2)*(T(8)-T(7))));
+qturb = -qcomp*cp_a(Tmoy)*(T(2)-T(1))/(n_meca*(cp_p(Tmoy2)*(Tn(8)-T(7))));
 
-ma = m3p*(1-f);
-Qcalo = ma * F * PCI*1000;
 qcarb = m3p*(1-f)*F*N/120*Ncyl;
-
+rapport_q_old = qwaste/qturb;
 qwaste = qcomp+qcarb-qturb;
-Dq = (qcomp+qcarb+qturb-qwaste)/(qcomp+qcarb+qturb)*100
 
+
+j = j+1;
+
+end
+
+Dq = (qwaste)/(qcomp+qcarb+qturb+qwaste)*100;
 
 %% 7 - 8'
 p8p = p_atm;
 T8p = Tn(7);
 
 %% 8' - 9
-% p(9) = p_atm;
-% Tn(9) = (1-qwaste/qmoteur)*Tn(8)+qwaste/qmoteur*T8p;
+qmoteur = qwaste + qturb;
+Tn(9) = qwaste/qmoteur*T8p + qturb/qmoteur*Tn(8);
 
-Tn(Tn==0)=T(Tn==0);
-pn(pn==0)=p(pn==0);
-Vn(Vn==0)=V(Vn==0);
-Tn=Tn'; pn=pn'; Vn=Vn';
-T=T'; p=p'; V=V';
+%% Calcul des travaux
+r3p = (1-f)*ra+f*rp;
+W3p4 = m3pn*r3p/(n_comp-1)*(Tn(4)-T3pn);
+Wp = -max(p_haut)*10^5*(V4pp-V_min);
+
+mr = 0.5*(m4ppn*r4pp+m5n*rp);
+Wt = -mr*Tn(5)*log(Vn(5)/V4pp);
+
+W56 = m5n*rp/(n_det-1)*(Tn(6)-Tn(5));
+
+W_hp = W3p4+Wp+Wt+W56;
+W_bp = (pn(7)-p3p)*10^5*(V_max-V_min);
+W_th = W_hp + W_bp;
+
+%% Bilan thermique
+ma = m3p*(1-f);
+Qcalo = ma * F * PCI*1000;
+Qcomb = n_comb * Qcalo;
+Qapp = Qv+Qp+Qt;
+
+%% Calcul des chaleurs
+Tmoy = 0.5*(Tn(2)+Tn(3));
+m_air = qcomp / (N/120 * Ncyl);
+Q23 = m_air * cp_a(Tmoy) * (Tn(3)-Tn(2)); % < 0
+
+Tmoy = 0.5*(T3pn+Tn(4));
+cv = f*cv_p(Tmoy)+(1-f)*cv_a(Tmoy);
+gamma = f*gamma_p(Tmoy)+(1-f)*gamma_a(Tmoy);
+Q3p4 = m3pn*cv*(Tn(4)-T3pn)-W3p4; % si n_comp = 1.34 < gamma = 1.37, Q < 0
+
+Tmoy = 0.5*(Tn(5)+Tn(6));
+cv = f*cv_p(Tmoy)+(1-f)*cv_a(Tmoy);
+gamma = f*gamma_p(Tmoy)+(1-f)*gamma_a(Tmoy);
+Q56 = m3pn*cv*(Tn(6)-Tn(5))-W56; % si n_det < gamma, Q > 0
+
+Q_pertes_comb = Qcomb-Qcalo; % < 0
+Q_pertes_paroi = Qapp - Qcomb; % <0
+
+Tmoy = 0.5*(Tn(7)+Tn(8));
+Wturb = qturb/(N/120*Ncyl) * cp_p(Tmoy) * (Tn(8)-Tn(7));
+Q_frottements = Wturb*(1-n_meca); % <0
+
+Tmoy =  0.5*(Tn(1)+T0);
+Tmoy2 = 0.5*(Tn(9)+T0);
+m_echap = qmoteur/(N/120*Ncyl);
+Q_echap = m_air * cp_a(Tmoy) * (Tn(1)-T0) - m_echap * cp_p(Tmoy2) * (Tn(9)-T0); % <0
+
+Pertes_moteur = Q_pertes_comb + Q_pertes_paroi + Q3p4 + Q56; % sans echap
+Pertes_turbo = Q23 + Q_frottements;
+
+indice = 1; %  1 -> total   2-> Pertes moteur   3-> Pertes turbo
+
+if (indice == 1)
+    X = [Pertes_moteur, Pertes_turbo, W_th, Q_echap];
+    labels = {'Pertes Moteur', 'Pertes Turbo', 'Travail Recup', 'Q echap'};
+elseif (indice ==2)
+    X = [Q_pertes_comb, Q_pertes_paroi, Q3p4, Q56, Q_echap];
+    labels = {'Q pertes comb', 'Q pertes paroi', 'Q3p4', 'Q56' ,'Q echap'};
+else 
+    X = [Q23, Q_frottements];
+    labels = {'Q23', 'Q frottements'};
+end
+
+% pie(abs(X))
+% legend(labels,'Location','southeast')
+
+
+% discuter comment diminuer les pertes
+
+DQ = (Qcalo-abs(Pertes_moteur+Q_echap+Pertes_turbo+W_th))/Qcalo*100;
+
+%% Performances
+
+Pi = abs(W_th) * N/120 * 4; %on a 4 cylindres
+PMI = abs(W_th)/(V_max-V_min); %on dit que W_th = Wi, c'est le cycle enveloppee
+PMI_hp = abs(W_hp)/(V_max-V_min);
+PMI_bp = abs(W_bp)/(V_max-V_min);
+CSI = qcarb/Pi;
+CSI_g = CSI*(1000*3600*1000);
+n_icycle = abs(W_th)/Qcalo;
+
+% 
+% n_th = -W_th / Qcomb;
+% n_cycle = W_i / W_th;
+% n_m = W_e / W_i; %mecanique de l'ensemble
+% n_i = n_comb * n_th * n_cycle;
+% n_eff = n_i * n_m;
+
+
+%% Temperature adiabatique de flamme 
+% prevoir les oxydes
+% vitesse de formation des oxydes
+
+% A partir du PCI
+qp = phi*PCI*1000/(phi+psi_s);
+T5 = Tn(4) + qp/cp_p(Tn(4));  
+
+DT = 100;
+tol = 1;                %DT = 1K
+i = 0;
+
+while (DT > tol && i < 10000)
+   i = i+1;
+   T5_old = T5;
+   Tmoy = 0.5*(Tn(4)+T5);
+   T5 = Tn(4) + qp/cp_p(Tmoy);
+   DT = abs(T5-T5_old);
+end
+
+
+% A partir de la chaleur de reaction
+
+
+
+
 
 
 %% Plot
-Vplot = [V_max; V_min; V_min; V4pp; Vn(5); V_max; V_max];
-pplot = [p3p; pn(4); p4p; p4pp; pn(5); pn(6); p3p];
+% Vplot = [V_max; V_min; V_min; V4pp; Vn(5); V_max; V_max];
+% pplot = [p3p; pn(4); p4p; p4pp; pn(5); pn(6); p3p];
 
 
-loglog(V_bas*V_min,p_bas,V_haut*V_min,p_haut, Vplot, pplot, '*')
+% loglog(V_bas*V_min,p_bas,V_haut*V_min,p_haut, Vplot, pplot, '*')
 % loglog(Vplot,pplot, '*')
-
 
