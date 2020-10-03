6fda4693ed9274c46f7e3bd11d30c5c000062154
FlavioHMSarti <63512638+FlavioHMSarti@users.noreply.github.com>
2020-10-03 23:14:06 +0200

    Add files via upload

---
+++ fonction_principale.m
@@ -0,0 +1,15 @@
+clear
+close
+clc
+
+opt_plot_1 = input('(1) Plot du camembert? (1) Oui / (0) Non \n')
+clc
+opt_plot_2 = input('(2) Plot du cycle enveloppe? (1) / (0) Non \n')
+clc
+opt_plot_3 = input('(3) Plot du cycle enveloppe log x log? (1) / (0) Non \n')
+clc
+
+addpath('./codes')
+addpath('./tables')
+
+code_matlab_modelisation
